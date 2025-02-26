b9c0f3c323f59bba7cd5dcd13d9b50a9eae9cbd1
wfstyer <wfstyer@gmail.com>
2025-02-26 14:39:45 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DataSet1 = New MTCDashboard.DataSet1()
+        Me.WorkcenterlistTableAdapter = New MTCDashboard.DataSet1TableAdapters.workcenterlistTableAdapter()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "DataSet1"
+        Me.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'WorkcenterlistTableAdapter
+        '
+        Me.WorkcenterlistTableAdapter.ClearBeforeFill = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1320, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(65, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Machines"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(1138, 35)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.Size = New System.Drawing.Size(247, 380)
+        Me.DataGridView1.TabIndex = 1
+        Me.DataGridView1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1393, 727)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "MTConnect DashBoard"
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataSet1 As DataSet1
+    Friend WithEvents WorkcenterlistTableAdapter As DataSet1TableAdapters.workcenterlistTableAdapter
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Public Machinelist As New DataTable
+    Public chosen As DataColumn = Machinelist.Columns.Add("Display", Type.GetType("System.Boolean"))
+    Public workcenter As DataColumn = Machinelist.Columns.Add("WCID", Type.GetType("System.String"))
+    Public descript As DataColumn = Machinelist.Columns.Add("Description", Type.GetType("System.String"))
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'DataSet1.workcenterlist' table. You can move, or remove it, as needed.
+        Me.WorkcenterlistTableAdapter.ClearBeforeFill = True
+        Me.WorkcenterlistTableAdapter.Fill(Me.DataSet1.workcenterlist)
+        'DataGridView1.Columns.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        'Machinelist.Clear()
+        Dim intCursor% = 0
+        Do Until intCursor = DataSet1.workcenterlist.Rows.Count
+            Dim workrow As DataRow = Machinelist.NewRow
+            workrow(chosen) = False
+            workrow(workcenter) = DataSet1.workcenterlist.Item(intCursor).WCID
+            workrow(descript) = DataSet1.workcenterlist.Item(intCursor).DESCRIPTION
+            Machinelist.Rows.Add(workrow)
+            intCursor += 1
+        Loop
+        DataGridView1.DataSource = Machinelist
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "Machines" Then
+            DataGridView1.Visible = True
+            Button1.Text = "Select"
+        Else
+            DataGridView1.Visible = False
+            Button1.Text = "Machines"
+        End If
+    End Sub
+
+End Class
